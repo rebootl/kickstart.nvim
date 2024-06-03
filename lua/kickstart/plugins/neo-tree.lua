48916de622a3b3db1004c79e1ce84a455a90052c
Cem Aydin <cem.aydin@gmx.ch>
2024-06-03 18:57:55 +0200

    neo tree show hidden

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -19,8 +19,13 @@ return {
         mappings = {
           ['\\'] = 'close_window',
           ["Z"] = "expand_all_nodes",
-          },
         },
+      },
+      filtered_items = {
+        visible = true, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = true,
+        hide_gitignored = true,
+      }
     },
   },
 }
